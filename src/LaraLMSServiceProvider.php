7677156fe4870b1c4187708a39d6fd47e08d2b83
Anushan iMac <anushanw@flatorb.com>
2018-10-04 18:04:25 +0530

    Initial plugin setup

---
+++ src/LaraLMSServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Flatorb\LaraLMS;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaraLMSServiceProvider extends ServiceProvider
+{
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		include __DIR__ . '/routes.php';
+		
+		$this->loadViewsFrom(__DIR__ . '/Views', 'laralms');
+	}
+	
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['laralms'] = $this->app->share(function($app) {
+			return new LaraLMS;
+		});
+	}
+}
